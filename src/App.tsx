16b227d3d5dae2e3f0041e29e94acb78df4cad0d
Benjamin SA <benjamynfrank101@gmail.com>
2024-11-22 14:29:06 +0100

    Add prettier + tailwindcss-prettier plugins
    - Remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className={"text-red-600"}>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="rounded-lg border border-solid bg-sky-700 px-4 py-2 text-center text-2xl text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hello Tables (data tables)
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
