1699c7ec336500da1c381ca1494a5cc57bba61fc
Benjamin SA <benjamynfrank101@gmail.com>
2024-11-24 05:18:15 +0100

    Add pagination controls

--- src/payments/data-table.tsx
+++ src/payments/data-table.tsx
@@ -15,6 +15,7 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { useState } from 'react';
+import { Button } from '@/components/ui/button.tsx';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -26,7 +27,7 @@ export function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const [pagination, setPagination] = useState({
-    pageIndex: 0,
+    pageIndex: 2,
     pageSize: 25,
   });
 
@@ -40,50 +41,103 @@ export function DataTable<TData, TValue>({
     },
   });
 
+  const goPreviousPage = () => {
+    setPagination((prevState) => ({
+      ...prevState,
+      pageIndex: prevState.pageIndex - 1,
+    }));
+  };
+
+  const goNextPage = () => {
+    setPagination((prevState) => ({
+      ...prevState,
+      pageIndex: prevState.pageIndex + 1,
+    }));
+  };
+
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} className="overflow-ellipsis">
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} className="overflow-ellipsis">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="overflow-ellipsis">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="overflow-ellipsis">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="pagination-ctr flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            if (table.getCanPreviousPage()) goPreviousPage();
+            // table.previousPage(); // only works if useState is not used
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <div className="page-indicator flex items-center space-x-2 py-4">
+          <p>Page {pagination.pageIndex + 1}</p>
+          <p>of</p>
+          <p>
+            {table.getPageCount()} - Total {table.getRowCount()}
+          </p>
+        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            if (table.getCanNextPage()) goNextPage();
+            // table.nextPage(); // works if state is not used
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
